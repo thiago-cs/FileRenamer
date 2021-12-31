2715052454044a8be23b9d976360f75fda2c2ab6
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2021-12-31 12:59:28 -0300

    Added support for using Regex.

--- FileRenamer.Core Tester/Test_ReplaceAction.cs
+++ FileRenamer.Core Tester/Test_ReplaceAction.cs
@@ -10,22 +10,25 @@ public sealed class Test_ReplaceAction
 
 	private const string quickBrownFox = "the quick brown fox jumps over the lazy dog.";
 	private const string fiveBoxingWizards = "The five boxing wizards jump quickly.";
-	//private const string loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at sem consectetur, egestas velit vitae, lacinia ipsum.";
-	//private const string neverForget = "Forget injuries, never forget kindnesses";
+	private const string loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at sem consectetur, egestas velit vitae, lacinia ipsum.";
+	private const string neverForget = "Forget injuries, never forget kindnesses";
 	private const string quickBrownNinjaCat = "the quick brown ninja cat jumps over the lazy dog.";
+	private const string regex1 = @",[^\.]*(?=,)";
 
 	#endregion
 
 
 	[Test]
-	[TestCase(quickBrownFox, "fox", "ninja cat", false, quickBrownNinjaCat)]
-	[TestCase(quickBrownFox, "fox", "ninja cat", true, quickBrownNinjaCat)]
-	[TestCase(quickBrownFox, "Fox", "ninja cat", false, quickBrownFox)]
-	[TestCase(quickBrownFox, "Fox", "ninja cat", true, quickBrownNinjaCat)]
-	[TestCase(fiveBoxingWizards, " ", ".", false, "The.five.boxing.wizards.jump.quickly.")]
-	public void AddToBeginning(string input, string oldString, string newString, bool ignoreCase, string expected)
+	[TestCase(quickBrownFox, "fox", "ninja cat", false, false, quickBrownNinjaCat)]
+	[TestCase(quickBrownFox, "fox", "ninja cat", true, false, quickBrownNinjaCat)]
+	[TestCase(quickBrownFox, "Fox", "ninja cat", false, false, quickBrownFox)]
+	[TestCase(quickBrownFox, "Fox", "ninja cat", true, false, quickBrownNinjaCat)]
+	[TestCase(fiveBoxingWizards, " ", ".", false, false, "The.five.boxing.wizards.jump.quickly.")]
+	[TestCase(loremIpsum, regex1, null, false, true, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at sem consectetur, lacinia ipsum.")]
+	[TestCase(neverForget, regex1, "", false, true, neverForget)]
+	public void Test(string input, string oldString, string newString, bool ignoreCase, bool useRegex, string expected)
 	{
-		Assert.AreEqual(expected, new ReplaceAction(oldString, newString, ignoreCase).Run(input));
+		Assert.AreEqual(expected, new ReplaceAction(oldString, newString, ignoreCase, useRegex).Run(input));
 		Assert.Pass();
 	}
 }
--- FileRenamer.Core/Actions/ReplaceAction.cs
+++ FileRenamer.Core/Actions/ReplaceAction.cs
@@ -1,22 +1,43 @@
-﻿namespace FileRenamer.Core.Actions;
+﻿using System.Text.RegularExpressions;
+
+
+namespace FileRenamer.Core.Actions;
 
 public sealed class ReplaceAction : RenameActionBase
 {
 	private readonly string oldString;
-	private readonly string newString;
+	private readonly string? newString;
 	private readonly bool ignoreCase;
+	private readonly bool useRegex;
+	private Regex? regex;
 
 
-	public ReplaceAction(string oldString, string newString, bool ignoreCase)
+	public ReplaceAction(string oldString, string? newString, bool ignoreCase, bool useRegex)
 	{
 		this.oldString = oldString;
 		this.newString = newString;
 		this.ignoreCase = ignoreCase;
+		this.useRegex = useRegex;
 	}
 
 
 	public override string Run(string input)
 	{
-		return input.Replace(oldString, newString, ignoreCase ? StringComparison.InvariantCultureIgnoreCase : StringComparison.InvariantCulture);
+		if (useRegex)
+		{
+			if (regex == null)
+			{
+				RegexOptions regexOptions = RegexOptions.CultureInvariant | RegexOptions.ExplicitCapture;
+
+				if (ignoreCase)
+					regexOptions |= RegexOptions.IgnoreCase;
+
+				regex = new Regex(oldString, regexOptions);
+			}
+
+			return regex.Replace(input, newString ?? "");
+		}
+		else
+			return input.Replace(oldString, newString, ignoreCase ? StringComparison.InvariantCultureIgnoreCase : StringComparison.InvariantCulture);
 	}
 }
