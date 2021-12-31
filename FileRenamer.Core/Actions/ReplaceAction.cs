4f2fabdb10d1d517385afd0e11d438ba3040893e
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2021-12-31 12:21:24 -0300

    Create ReplaceAction.cs

---
+++ FileRenamer.Core/Actions/ReplaceAction.cs
@@ -0,0 +1,22 @@
+﻿namespace FileRenamer.Core.Actions;
+
+public sealed class ReplaceAction : RenameActionBase
+{
+	private readonly string oldString;
+	private readonly string newString;
+	private readonly bool ignoreCase;
+
+
+	public ReplaceAction(string oldString, string newString, bool ignoreCase)
+	{
+		this.oldString = oldString;
+		this.newString = newString;
+		this.ignoreCase = ignoreCase;
+	}
+
+
+	public override string Run(string input)
+	{
+		return input.Replace(oldString, newString, ignoreCase ? StringComparison.InvariantCultureIgnoreCase : StringComparison.InvariantCulture);
+	}
+}
