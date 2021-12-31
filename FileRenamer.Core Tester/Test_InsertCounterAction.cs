59d82b1ecdd4de3adbecdc175d013e14e162d558
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2021-12-31 18:23:44 -0300

    Created tests for InsertCounterAction

---
+++ FileRenamer.Core Tester/Test_InsertCounterAction.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using FileRenamer.Core.Indices;
+using FileRenamer.Core.Actions;
+
+
+namespace FileRenamer.Core_Tester;
+
+public sealed class Test_InsertCounterAction
+{
+	[Test]
+	[TestCase("new document ().txt", 14, 1, 1, new string[] { "new document (1).txt", "new document (2).txt", "new document (3).txt" })]
+	[TestCase("big file.part", 13, 0, 3, new string[] { "big file.part000", "big file.part001", "big file.part002", "big file.part003", })]
+	public void Test(string input, int insertPosition, int startValue, int width, string[] expected)
+	{
+		InsertCounterAction insertCounterAction = new(new FixedIndexFinder(insertPosition), startValue, width);
+
+		for (int i = 0; i < expected.Length; i++)
+			Assert.AreEqual(expected[i], insertCounterAction.Run(input));
+
+		Assert.Pass();
+	}
+}
