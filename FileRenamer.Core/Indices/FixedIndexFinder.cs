8e3806ef646224dc548b471b013ffcfd03d35b52
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2021-12-30 21:00:39 -0300

    Create FixedIndexFinder.cs

---
+++ FileRenamer.Core/Indices/FixedIndexFinder.cs
@@ -0,0 +1,18 @@
+﻿namespace FileRenamer.Core.Indices;
+
+public sealed class FixedIndexFinder : IIndexFinder
+{
+	private readonly int index;
+
+
+	public FixedIndexFinder(int value)
+	{
+		index = value;
+	}
+
+
+	public int FindIn(string input)
+	{
+		return index;
+	}
+}
