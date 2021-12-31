b8543430484b20e3a867b4b549611a113a67016f
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2021-12-30 21:52:46 -0300

    Create FileExtensionIndexFinder.cs

---
+++ FileRenamer.Core/Indices/FileExtensionIndexFinder.cs
@@ -0,0 +1,16 @@
+﻿namespace FileRenamer.Core.Indices;
+
+public sealed class FileExtensionIndexFinder : IIndexFinder
+{
+	public int FindIn(string input)
+	{
+		for (int i = input.Length - 1; i >= 0; i--)
+			switch (input[i])
+			{
+				case '.': return i;
+				case ' ': return -1;
+			}
+
+		return -1;
+	}
+}
