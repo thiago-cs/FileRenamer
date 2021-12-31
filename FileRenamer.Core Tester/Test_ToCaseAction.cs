8a17bd91e9c1cdaa7267fb3eff62266e574b396a
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2021-12-31 15:50:16 -0300

    Create Test_ToCaseAction.cs

---
+++ FileRenamer.Core Tester/Test_ToCaseAction.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using FileRenamer.Core.Indices;
+using FileRenamer.Core.Actions;
+using FileRenamer.Core.Extensions;
+
+
+namespace FileRenamer.Core_Tester;
+
+public sealed class Test_ToCaseAction
+{
+	[Test]
+	[TestCase("new document.txt", true, TextCasing.TitleCase, "New Document.txt")]
+	[TestCase("new document.txt", false, TextCasing.UpperCase, "new document.TXT")]
+	[TestCase("Area.effect", true, TextCasing.LowerCase, "area.effect")]
+	public void FileNameTest(string fileName, bool convertName, TextCasing casing, string expected)
+	{
+		FileExtensionIndexFinder extensionIndexFinder = new();
+		IIndexFinder startIndexFinder;
+		IIndexFinder endIndexFinder;
+
+		if (convertName)
+		{
+			startIndexFinder = new BeginningIndexFinder();
+			endIndexFinder = extensionIndexFinder;
+		}
+		else
+		{
+			startIndexFinder = extensionIndexFinder;
+			endIndexFinder  = new EndIndexFinder();
+		}
+
+		Assert.AreEqual(expected, new ToCaseAction(startIndexFinder, endIndexFinder, casing).Run(fileName));
+		Assert.Pass();
+	}
+}
