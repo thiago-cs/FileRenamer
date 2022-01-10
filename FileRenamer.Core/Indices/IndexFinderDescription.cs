610cdb94f1af54f552adcb31792c41f5e1f1f34f
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-01-10 17:36:15 -0300

    Created IndexFinderDescription.

---
+++ FileRenamer.Core/Indices/IndexFinderDescription.cs
@@ -0,0 +1,21 @@
+﻿namespace FileRenamer.Core.Indices;
+
+public sealed class IndexFinderDescription
+{
+	public string StartPrepositon { get; }
+	public string Description { get; }
+
+
+	public IndexFinderDescription(string startPrepositon, string description)
+	{
+		StartPrepositon = startPrepositon;
+		Description = description;
+	}
+
+	internal object ToString(bool includePreposition)
+	{
+		return includePreposition && !string.IsNullOrWhiteSpace(StartPrepositon)
+			? $"{StartPrepositon} {Description}"
+			: Description;
+	}
+}
