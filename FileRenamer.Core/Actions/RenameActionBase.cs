05ce03526c0c761f1021c7117496c017220144db
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-01-10 22:07:01 -0300

    Amend commit "Added Description to InsertAction".
    The changes in file 'RenameActionBase' should have been included in commit d0a1a0c.
    Without the changes introduced in this commit, the code will not run.

--- FileRenamer.Core/Actions/RenameActionBase.cs
+++ FileRenamer.Core/Actions/RenameActionBase.cs
@@ -1,10 +1,12 @@
 ﻿namespace FileRenamer.Core.Actions;
 
-
 public abstract class RenameActionBase : Microsoft.Toolkit.Mvvm.ComponentModel.ObservableObject
 {
 	private bool _isEnabled = true;
 	public bool IsEnabled { get => _isEnabled; set => SetProperty(ref _isEnabled, value); }
+
+	private string _description = "";
+	public string Description { get => _description; set => SetProperty(ref _description, value); }
 
 
 	public abstract string Run(string input);
