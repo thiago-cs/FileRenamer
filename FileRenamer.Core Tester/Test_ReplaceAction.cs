e987963b702a705aa7d727fa301d6f1699fcd469
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-01-15 20:59:53 -0300

    Added the ability to specify the range within which the replacement operation is to take place.
    ...and its related tests.

--- FileRenamer.Core Tester/Test_ReplaceAction.cs
+++ FileRenamer.Core Tester/Test_ReplaceAction.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using FileRenamer.Core.Indices;
 using FileRenamer.Core.Actions;
 using static FileRenamer.Core_Tester.Resources;
 
@@ -16,17 +17,37 @@ public sealed class Test_ReplaceAction
 	[TestCase(fiveBoxingWizards, " ", ".", false, false, "The.five.boxing.wizards.jump.quickly.")]
 	[TestCase(loremIpsum, regex1, null, false, true, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at sem consectetur, lacinia ipsum.")]
 	[TestCase(neverForget, regex1, "", false, true, neverForget)]
-	public void TestRun(string input, string oldString, string newString, bool ignoreCase, bool useRegex, string expected)
+	public void TestWithoutBounds(string input, string oldString, string newString, bool ignoreCase, bool useRegex, string expected)
 	{
 		Assert.AreEqual(expected, new ReplaceAction(oldString, newString, ignoreCase, useRegex).Run(input));
 	}
 
 
 	[Test]
+	[TestCase(quickBrownFox, 05, 10, "fox", "ninja cat", false, false, quickBrownFox)]
+	[TestCase(quickBrownFox, 15, 20, "fox", "ninja cat", false, false, quickBrownNinjaCat)]
+	[TestCase(quickBrownFox, 09, 16, /*language=regex*/ @"\s", ".", false, true, "the quick.brown.fox jumps over the lazy dog.")]
+	[TestCase("sun of a beach", 10, 12, /*language=regex*/ @"\w", "*", false, true, "sun of a b**ch")]
+	public void TestWithBounds(string input, int start, int end, string oldString, string newString, bool ignoreCase, bool useRegex, string expected)
+	{
+		Assert.AreEqual(expected, new ReplaceAction(new FixedIndexFinder(start), new FixedIndexFinder(end), oldString, newString, ignoreCase, useRegex).Run(input));
+	}
+
+
+	[Test]
 	[TestCase("fox", "ninja cat", false, @"replace ""fox"" with ""ninja cat""")]
 	[TestCase(regex1, "new text", true, @"replace the expression "",[^\.]*(?=,)"" with ""new text""")]
-	public void TestDescription(string oldString, string newString, bool useRegex, string expected)
+	public void TestDescriptionWithoutBounds(string oldString, string newString, bool useRegex, string expected)
 	{
 		Assert.AreEqual(expected, new ReplaceAction(oldString, newString, false, useRegex).Description);
+	}
+
+
+	[Test]
+	[TestCase(0, 5, "fox", "ninja cat", false, @"replace ""fox"" within file name with ""ninja cat""")]
+	[TestCase(2, 4, regex1, "new text", true, @"replace the expression "",[^\.]*(?=,)"" within characters from before ""sunset"" to after the expression ""(Hi|Hello) kitty"" with ""new text""")]
+	public void TestDescriptionWithBounds(int i1, int i2, string oldString, string newString, bool useRegex, string expected)
+	{
+		Assert.AreEqual(expected, new ReplaceAction(finders[i1], finders[i2], oldString, newString, false, useRegex).Description);
 	}
 }
--- FileRenamer.Core/Actions/ReplaceAction.cs
+++ FileRenamer.Core/Actions/ReplaceAction.cs
@@ -1,10 +1,13 @@
 ﻿using System.Text.RegularExpressions;
+using FileRenamer.Core.Indices;
 
 
 namespace FileRenamer.Core.Actions;
 
 public sealed class ReplaceAction : RenameActionBase
 {
+	private readonly IIndexFinder? startIndexFinder;
+	private readonly IIndexFinder? endIndexFinder;
 	private readonly string oldString;
 	private readonly string? newString;
 	private readonly bool ignoreCase;
@@ -14,11 +17,13 @@ public sealed class ReplaceAction : RenameActionBase
 
 	public ReplaceAction(string oldString, string? newString, bool ignoreCase, bool useRegex)
 	{
-		this.oldString = oldString;
+		// 1. 
+		this.oldString = oldString ?? throw new ArgumentNullException(nameof(oldString));
 		this.newString = newString;
 		this.ignoreCase = ignoreCase;
 		this.useRegex = useRegex;
 
+		// 2.
 		System.Text.StringBuilder sb = new("replace ");
 
 		if (this.useRegex)
@@ -36,8 +41,62 @@ public sealed class ReplaceAction : RenameActionBase
 		Description = sb.ToString();
 	}
 
+	public ReplaceAction(IIndexFinder startIndexFinder, IIndexFinder endIndexFinder, string oldString, string? newString, bool ignoreCase, bool useRegex)
+	{
+		// 1. 
+		this.startIndexFinder = startIndexFinder ?? throw new ArgumentNullException(nameof(startIndexFinder));
+		this.endIndexFinder = endIndexFinder ?? throw new ArgumentNullException(nameof(endIndexFinder));
+		this.oldString = oldString ?? throw new ArgumentNullException(nameof(oldString));
+		this.newString = newString;
+		this.ignoreCase = ignoreCase;
+		this.useRegex = useRegex;
+
+		// 2. 
+		System.Text.StringBuilder sb = new("replace ");
+
+		if (this.useRegex)
+			sb.Append("the expression ");
+
+		sb.Append('"')
+		  .Append(this.oldString)
+		  .Append(@""" within ")
+		  .Append(Helpers.DescriptionHelper.GetRangeFriendlyName(this.startIndexFinder, this.endIndexFinder))
+		  .Append(@" with """);
+
+		if (this.newString != null)
+			sb.Append(this.newString);
+
+		sb.Append('"');
+
+		Description = sb.ToString();
+	}
+
 
 	public override string Run(string input)
+	{
+		// 
+		if (startIndexFinder == null || endIndexFinder == null)
+			return Run_Core(input);
+
+		//
+		int startIndex = startIndexFinder.FindIn(input);
+
+		if (startIndex == -1)
+			return input;
+
+		int endIndex = endIndexFinder.FindIn(input);
+
+		if (endIndex == -1)
+			return input;
+
+		if (endIndex < startIndex)
+			return input;
+
+		// 
+		return input[..startIndex] + Run_Core(input[startIndex..endIndex]) + input[endIndex..];
+	}
+
+	private string Run_Core(string input)
 	{
 		if (useRegex)
 		{
