e9a08947840f00ade53c0b6792558245478182fb
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-01-19 17:01:57 -0300

    Created a test for ActionCollection.Run() method.

---
+++ FileRenamer.Core Tester/Test_ActionCollection.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using FileRenamer.Core;
+using FileRenamer.Core.Actions;
+using FileRenamer.Core.Indices;
+using FileRenamer.Core.Extensions;
+
+
+namespace FileRenamer.Core_Tester;
+
+[TestFixture]
+public static class Test_ActionCollection
+{
+	[Test]
+	public static void TestRun1()
+	{
+		const string input = "y2mate.com - Task vs ValueTask When Should I use ValueTask_1080p.MP4";
+		const string expected = "Task vs. ValueTask When Should I use ValueTask - Brian Lagunas.mp4";
+
+		ActionCollection actions = new()
+		{
+			new RemoveAction(new BeginningIndexFinder(), new SubstringIndexFinder(" - ", false, false, false)),
+			new InsertAction(new SubstringIndexFinder("vs", false, false, false), "."),
+			new ReplaceAction(@"_\d*p", " - Brian Lagunas", false, true),
+			new ToCaseAction(new FileExtensionIndexFinder(), new EndIndexFinder(), TextCasing.LowerCase),
+		};
+
+		Assert.AreEqual(expected, actions.Run(input));
+	}
+}
