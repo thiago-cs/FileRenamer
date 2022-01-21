7e576cd42513671490024e06f41b6d2d7d343836
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-01-20 22:45:26 -0300

    Implemented Run() method.

--- FileRenamer.Core/Project.cs
+++ FileRenamer.Core/Project.cs
@@ -5,23 +5,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FileRenamer.Core.Actions;
+using FileRenamer.Core.FileSystem;
 
 
 namespace FileRenamer.Core;
 
 public sealed class Project : Microsoft.Toolkit.Mvvm.ComponentModel.ObservableObject
 {
-	public readonly ActionCollection _actions = new();
+	public ActionCollection Actions { get; } = new();
 
-	private string? _folderPath;
-	public string? FolderPath { get => _folderPath; set => SetProperty(ref _folderPath, value); }
+	/// <summary>
+	/// Gets or sets the current working directory on which file operations are run.
+	/// </summary>
+	public IFolder? Folder { get => _folder; set => SetProperty(ref _folder, value); }
+	private IFolder? _folder;
+
+	/// <summary>
+	/// Gets a value between 0 and 100 representing the progress of the ongoing operation.
+	/// </summary>
+	public double Progress { get => _progress; private set => SetProperty(ref _progress, value); }
+	private double _progress;
 
 
-	public async Task Run()
+	public async Task Run(CancellationToken cancellationToken, bool parallel = false)
 	{
-		if (_folderPath == null)
+		// 0. 
+		if (Folder == null)
+		{
+			//Error("Folder is null.");
 			return;
+		}
 
-		await Task.CompletedTask;
+		// 1. 
+		string[] fileNames = await Folder.GetFileNamesAsync();
+		int counter = 0;
+
+		if (parallel)
+			await Parallel.ForEachAsync(fileNames, cancellationToken, RenameFile);
+		else
+			foreach (string fileName in fileNames)
+			{
+				if (cancellationToken.IsCancellationRequested)
+					break;
+
+				await Folder.RenameFileAsync(fileName, Actions.Run(fileName)).ConfigureAwait(true);
+				counter++;
+			}
+
+
+
+		async ValueTask RenameFile(string fileName, CancellationToken _cancellationToken)
+		{
+			await Folder!.RenameFileAsync(fileName, Actions.Run(fileName)).ConfigureAwait(true);
+			Interlocked.Add(ref counter, 1);
+			Progress = counter;
+		}
 	}
 }
