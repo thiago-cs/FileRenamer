8e1731f0fd49711b4a86c34b83d0c04b3654d7b4
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-01-23 03:44:53 -0300

    Created Toolbar.

---
+++ FileRenamer/UserControls/Toolbar.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace FileRenamer.UserControls;
+
+[Microsoft.UI.Xaml.Markup.ContentProperty(Name = nameof(Groups))]
+public sealed partial class Toolbar
+{
+	public System.Collections.Generic.ObservableVector<ToolbarGroup> Groups { get; } = new();
+
+
+	public Toolbar()
+	{
+		InitializeComponent();
+	}
+}
