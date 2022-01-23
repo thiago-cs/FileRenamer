06503f49d0163fc07fd20053d2e30fc8038324de
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-01-22 21:38:40 -0300

    Moved MainWindowViewModel to correct folder.

--- FileRenamer/MainWindow.xaml.cs
+++ FileRenamer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using FileRenamer.ViewModels;
 
 
 namespace FileRenamer;
--- FileRenamer/ViewModels/MainWindowViewModel.cs
+++ FileRenamer/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FileRenamer.Core;
+﻿using FileRenamer.Core;
 
 
-namespace FileRenamer;
+namespace FileRenamer.ViewModels;
 
 public sealed class MainWindowViewModel : Microsoft.Toolkit.Mvvm.ComponentModel.ObservableObject
 {
