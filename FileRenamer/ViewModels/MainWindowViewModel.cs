b0e0cb1384748fbbe6d7cfd135a34aadb68270c2
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-01-24 05:32:22 -0300

    Added SelectedAction property.

--- FileRenamer/ViewModels/MainWindowViewModel.cs
+++ FileRenamer/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using FileRenamer.Core;
+using FileRenamer.Core.Actions;
 
 
 namespace FileRenamer.ViewModels;
 
 public sealed class MainWindowViewModel : Microsoft.Toolkit.Mvvm.ComponentModel.ObservableObject
 {
-	private Project _project;
+	private Project _project = new();
 	public Project Project { get => _project; set => SetProperty(ref _project, value); }
+
+	private RenameActionBase _selectedAction;
+	public RenameActionBase SelectedAction { get => _selectedAction; set => SetProperty(ref _selectedAction, value); }
 }
