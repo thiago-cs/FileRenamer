0cfcd79fbcacb792f348cdb0f468c900ec08939b
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-01-25 01:20:24 -0300

    Added result preview for the actions in the project.

--- FileRenamer/ViewModels/MainWindowViewModel.cs
+++ FileRenamer/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,104 @@
 ﻿using FileRenamer.Core;
 using FileRenamer.Core.Actions;
-
+using System;
+using System.Collections.Specialized;
+using System.ComponentModel;
 
 namespace FileRenamer.ViewModels;
 
 public sealed class MainWindowViewModel : Microsoft.Toolkit.Mvvm.ComponentModel.ObservableObject
 {
-	private Project _project = new();
-	public Project Project { get => _project; set => SetProperty(ref _project, value); }
+	private Project _project;
+	public Project Project
+	{
+		get => _project;
+		set
+		{
+			if (_project != null)
+				_project.Actions.CollectionChanged -= Actions_CollectionChanged;
+
+			if (SetProperty(ref _project, value))
+				UpdateTestOutput();
+
+			if (_project != null)
+				_project.Actions.CollectionChanged += Actions_CollectionChanged;
+		}
+	}
 
 	private RenameActionBase _selectedAction;
 	public RenameActionBase SelectedAction { get => _selectedAction; set => SetProperty(ref _selectedAction, value); }
+
+
+	public MainWindowViewModel()
+	{
+		Project = new();
+	}
+
+
+	#region Test lab
+
+	private string _testInput;
+	public string TestInput
+	{
+		get => _testInput;
+		set
+		{
+			if (SetProperty(ref _testInput, value))
+				UpdateTestOutput();
+		}
+	}
+
+	private string _testOutput;
+	public string TestOutput { get => _testOutput; private set => SetProperty(ref _testOutput, value); }
+
+	private void UpdateTestOutput()
+	{
+		TestOutput = TestInput is not null
+			? Project?.Actions.Run(TestInput)
+			: null;
+	}
+
+	#endregion
+
+	private void Actions_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+	{
+		// 1.
+		UpdateTestOutput();
+
+		// 2. 
+		switch (e.Action)
+		{
+			case NotifyCollectionChangedAction.Add:
+				foreach (RenameActionBase action in e.NewItems)
+					action.PropertyChanged += Action_PropertyChanged;
+
+				break;
+
+			case NotifyCollectionChangedAction.Reset:
+			case NotifyCollectionChangedAction.Remove:
+				foreach (RenameActionBase action in e.OldItems)
+					action.PropertyChanged -= Action_PropertyChanged;
+
+				break;
+
+			case NotifyCollectionChangedAction.Replace:
+				foreach (RenameActionBase action in e.NewItems)
+					action.PropertyChanged += Action_PropertyChanged;
+
+				foreach (RenameActionBase action in e.OldItems)
+					action.PropertyChanged -= Action_PropertyChanged;
+
+				break;
+
+			case NotifyCollectionChangedAction.Move:
+			default:
+				break;
+		}
+	}
+
+	private void Action_PropertyChanged(object sender, PropertyChangedEventArgs e)
+	{
+		UpdateTestOutput();
+	}
+
 }
