81b2d60e9b9278ae88a5f3fdb9ce43f4b502fbb2
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-01-31 01:02:02 -0300

    Create StringSourceType.cs

---
+++ FileRenamer/UserControls/InputControls/StringSourceType.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+
+namespace FileRenamer.UserControls.InputControls;
+
+public enum StringSourceType
+{
+	[Description("text")]
+	Text,
+
+	[Description("a counter")]
+	Counter,
+
+	//[Description("MP3 tag")]
+	//Mp3Tag,
+
+	//[Description("EXIF tag")]
+	//ExifTag,
+}
