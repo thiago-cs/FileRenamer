65455c06747ce3b4ecd92129e80332d956204c5b
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-02-03 04:02:20 -0300

    Created SearchTextEditor user control.

---
+++ FileRenamer/UserControls/InputControls/SearchTextEditor.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+
+namespace FileRenamer.UserControls.InputControls;
+
+public sealed partial class SearchTextEditor : UserControl
+{
+	#region Data DependencyProperty
+	public SearchTextData Data
+	{
+		get => (SearchTextData)GetValue(DataProperty);
+		set => SetValue(DataProperty, value);
+	}
+
+	// Using a DependencyProperty as the backing store for Data.  This enables animation, styling, binding, etc...
+	public static readonly DependencyProperty DataProperty =
+		DependencyProperty.Register(
+			nameof(Data),
+			typeof(SearchTextData),
+			typeof(SearchTextEditor),
+			new PropertyMetadata(null));
+	#endregion Data DependencyProperty
+
+
+	public SearchTextEditor()
+	{
+		InitializeComponent();
+	}
+}
