2a813030ba2675d04bc9488af23bfc8388d0c26b
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-02-03 04:11:57 -0300

    Using SearchTextData to handle text data.

--- FileRenamer/UserControls/InputControls/IndexFinderEditorData.cs
+++ FileRenamer/UserControls/InputControls/IndexFinderEditorData.cs
@@ -35,32 +35,7 @@ public sealed class IndexFinderEditorData : System.ComponentModel.BindableBase
 
 	#region After | Before
 
-	private TextType _textType;
-	public TextType TextType
-	{
-		get => _textType;
-		set
-		{
-			if (SetProperty(ref _textType, value))
-				Validate();
-		}
-	}
-
-	private string _text;
-	public string Text
-	{
-		get => _text;
-		set
-		{
-			if (SetProperty(ref _text, value))
-				Validate();
-		}
-	}
-
-	private string _textError;
-	public string TextError { get => _textError; private set => SetProperty(ref _textError, value); }
-
-	public bool IgnoreCase { get; set; }
+	public SearchTextData SearchTextData { get; } = new();
 
 	#endregion
 
@@ -68,6 +43,7 @@ public sealed class IndexFinderEditorData : System.ComponentModel.BindableBase
 	public IndexFinderEditorData()
 	{
 		Validate();
+		SearchTextData.PropertyChanged += SearchTextData_PropertyChanged;
 	}
 
 
@@ -82,8 +58,8 @@ public sealed class IndexFinderEditorData : System.ComponentModel.BindableBase
 				IndexFinderType.End => new EndIndexFinder(),
 				IndexFinderType.FileExtension => new FileExtensionIndexFinder(),
 				IndexFinderType.Position => new FixedIndexFinder(IndexPosition),
-				IndexFinderType.Before => new SubstringIndexFinder(Text, true, IgnoreCase, TextType == TextType.Regex),
-				IndexFinderType.After => new SubstringIndexFinder(Text, true, IgnoreCase, TextType == TextType.Regex),
+				IndexFinderType.Before => new SubstringIndexFinder(SearchTextData.Text, before: true, SearchTextData.IgnoreCase, SearchTextData.TextType == TextType.Regex),
+				IndexFinderType.After => new SubstringIndexFinder(SearchTextData.Text, before: false, SearchTextData.IgnoreCase, SearchTextData.TextType == TextType.Regex),
 				_ => throw new System.NotImplementedException($"Unknown {nameof(IIndexFinder)} type '{IndexType}'."),
 			};
 	}
@@ -94,42 +70,18 @@ public sealed class IndexFinderEditorData : System.ComponentModel.BindableBase
 	{
 		// 1. 
 		string indexTypeError = null;
-		string textError = null;
 
-		switch (IndexType)
-		{
-			case IndexFinderType.None:
-				indexTypeError = "Select index type.";
-				break;
-
-			case IndexFinderType.Before:
-			case IndexFinderType.After:
-				if (TextType == TextType.Text)
-				{
-					if (string.IsNullOrEmpty(Text))
-						textError = "Enter a text.";
-				}
-				else if (TextType == TextType.Regex)
-				{
-					if (string.IsNullOrEmpty(Text))
-						textError = "Enter a pattern.";
-					else if (!IsRegexPatternValid(Text))
-						textError = "Enter a valid pattern.";
-				}
-				break;
-
-			case IndexFinderType.Beginning:
-			case IndexFinderType.End:
-			case IndexFinderType.FileExtension:
-			case IndexFinderType.Position:
-			default:
-				break;
-		}
+		if (IndexType == IndexFinderType.None)
+			indexTypeError = "Select an index type.";
 
 		// 2. 
 		IndexTypeError = indexTypeError;
-		TextError = textError;
-		HasErrors = IndexTypeError != null || TextError != null;
+		UpdateHasErrors();
+	}
+
+	private void UpdateHasErrors()
+	{
+		HasErrors = IndexTypeError != null || (IndexType is IndexFinderType.Before or IndexFinderType.After && SearchTextData.HasErrors);
 	}
 
 	private static bool IsRegexPatternValid(string pattern)
@@ -146,4 +98,11 @@ public sealed class IndexFinderEditorData : System.ComponentModel.BindableBase
 	}
 
 	#endregion
+
+
+	private void SearchTextData_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+	{
+		if (e.PropertyName == nameof(SearchTextData.HasErrors))
+			UpdateHasErrors();
+	}
 }
