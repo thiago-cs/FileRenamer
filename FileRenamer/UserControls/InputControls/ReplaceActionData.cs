1ccc795fd2562e3a669e84d1c74d0a03fd081d17
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-02-04 01:10:56 -0300

    Created ReplaceActionData.

---
+++ FileRenamer/UserControls/InputControls/ReplaceActionData.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.ComponentModel;
+
+
+namespace FileRenamer.UserControls.InputControls;
+
+public sealed class ReplaceActionData : BindableBase
+{
+	public static readonly ExecutionScope[] executionScopeTypes = { ExecutionScope.WholeInput, ExecutionScope.Range };
+
+
+	private bool _hasErrors;
+	public new bool HasErrors { get => _hasErrors; private set => SetProperty(ref _hasErrors, value); }
+
+	public SearchTextData OldString { get; } = new();
+
+	public string NewString { get; set; }
+
+	private ExecutionScope _executionScope;
+	public ExecutionScope ExecutionScope
+	{
+		get => _executionScope;
+		set
+		{
+			if (SetProperty(ref _executionScope, value))
+				Validate();
+		}
+	}
+
+	public IndexFinderEditorData StartIndexData { get; } = new();
+
+	public IndexFinderEditorData EndIndexData { get; } = new();
+
+	private string _endIndexError;
+	public string EndIndexError
+	{
+		get => _endIndexError;
+		set
+		{
+			if (SetProperty(ref _endIndexError, value))
+				Validate();
+		}
+	}
+
+
+	public ReplaceActionData()
+	{
+		Validate();
+		StartIndexData.PropertyChanged += IndexData_PropertyChanged;
+		EndIndexData.PropertyChanged += IndexData_PropertyChanged;
+		OldString.PropertyChanged += OldString_PropertyChanged;
+	}
+
+
+	#region Validation
+
+	private void Validate()
+	{
+		//// 1. 
+		//string indexTypeError = null;
+
+		//if (IndexType == IndexFinderType.None)
+		//	indexTypeError = "Select an index type.";
+
+		//// 2. 
+		//IndexTypeError = indexTypeError;
+		UpdateHasErrors();
+	}
+
+	private void UpdateHasErrors()
+	{
+		HasErrors = OldString.HasErrors || 
+					(ExecutionScope == ExecutionScope.Range && (StartIndexData.HasErrors || EndIndexData.HasErrors || EndIndexError != null));
+	}
+
+	#endregion
+
+
+	private void IndexData_PropertyChanged(object sender, PropertyChangedEventArgs e)
+	{
+		switch (e.PropertyName)
+		{
+			case nameof(BindableBase.HasErrors):
+				UpdateHasErrors();
+				break;
+
+			case nameof(IndexFinderEditorData.IndexType):
+			case nameof(IndexFinderEditorData.IndexPosition):
+				Validate();
+				break;
+		}
+	}
+
+	private void OldString_PropertyChanged(object sender, PropertyChangedEventArgs e)
+	{
+		if (e.PropertyName == nameof(BindableBase.HasErrors))
+			UpdateHasErrors();
+	}
+}
