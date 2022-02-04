349389ee79b5f8e714b53b5a2759d34ad72da892
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-02-04 01:10:02 -0300

    Created ExecutionScope.

---
+++ FileRenamer/UserControls/InputControls/ExecutionScope.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+
+namespace FileRenamer.UserControls.InputControls;
+
+public enum ExecutionScope
+{
+	[Description("everything")]
+	WholeInput,
+
+	[Description("range")]
+	Range,
+
+	[Description("all occurrencies of")]
+	Occurrences,
+}
