e55cd2b3a769b994143e9d1398bf23e3ef7dfbfd
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-02-09 04:08:47 -0300

    Negative values for the index are now accepted.
    * Negative values mean the count should start from the last character.
    * Improved description (definite articles are not used as often in English as I thought).

--- FileRenamer.Core/Indices/FixedIndex.cs
+++ FileRenamer.Core/Indices/FixedIndex.cs
@@ -8,7 +8,21 @@ public sealed class FixedIndex : IIndex
 	private readonly int index;
 
 
-	public IndexDescription Description => new("after", $"the {index.Ordinalize(GrammaticalGender.Neuter)} character");
+	public IndexDescription Description
+	{
+		get
+		{
+			const string startPrepositon = "before the";
+
+			return index switch
+			{
+				< -1 => new(startPrepositon, $"{(-index).Ordinalize(GrammaticalGender.Neuter)} to last character"),
+				-1 => new(startPrepositon, "last character"),
+				0 => new(startPrepositon, "first character"),
+				_ => new("after the", $"{index.Ordinalize(GrammaticalGender.Neuter)} character")
+			};
+		}
+	}
 
 
 	public FixedIndex(int value)
@@ -21,6 +35,6 @@ public sealed class FixedIndex : IIndex
 
 	public int FindIn(string input)
 	{
-		return index;
+		return index < 0 ? input.Length + index : index;
 	}
 }
