8e209f490ece8d620f8aa62bc8f80afb4266dd9f
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-06-04 12:43:31 -0300

    Add IItem.

---
+++ FileRenamer.Core/FileSystem/IItem.cs
@@ -0,0 +1,28 @@
+﻿namespace FileRenamer.Core.FileSystem;
+
+public interface IItem
+{
+	/// <summary>
+	/// Gets the full path of this item.
+	/// </summary>
+	public string Path { get; }
+
+	/// <summary>
+	/// Gets the name of this item.
+	/// </summary>
+	public string Name => System.IO.Path.GetFileName(Path);
+
+	/// <summary>
+	/// Renames this item.
+	/// </summary>
+	/// <param name="newName">The desired, new name of this item.</param>
+	/// <returns>If this method completes successfully, <see langword="true"/>. Otherwise <see langword="false"/>.</returns>
+	Task<bool> RenameAsync(string newName);
+
+	/// <summary>
+	/// Moves this item into a new folder.
+	/// </summary>
+	/// <param name="newFolder">The new parent folder of this item.</param>
+	/// <returns>If this method completes successfully, <see langword="true"/>. Otherwise <see langword="false"/>.</returns>
+	public Task<bool> MoveAsync(IFolder newFolder);
+}
