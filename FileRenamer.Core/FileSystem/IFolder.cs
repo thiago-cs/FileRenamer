052b7b9df07b4d4477c48a162f8602163597478f
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-06-04 12:53:52 -0300

    Added inheritance to IItem

--- FileRenamer.Core/FileSystem/IFolder.cs
+++ FileRenamer.Core/FileSystem/IFolder.cs
@@ -4,33 +4,26 @@
 /// Represents a folder in the target Operating System's file system.
 /// Provides information about the folder and its content, and ways to manipulate them.
 /// </summary>
-public interface IFolder
+public interface IFolder : IItem
 {
-	/// <summary>
-	/// Gets the full path of the current folder in the file system.
-	/// </summary>
-	public string Path { get; }
-
-
 	/// <summary>
 	/// Gets the subfolders in the current folder.
 	/// </summary>
 	/// <remarks>This method performs a shallow query that returns only subfolders in the current folder.</remarks>
-	/// <returns>When this method completes successfully, it returns a list of the subfolders in the current folder.</returns>
+	/// <returns>When this method completes, it returns a list of the subfolders in the current folder.</returns>
 	public Task<IFolder[]> GetSubfoldersAsync();
 
 	/// <summary>
 	/// Gets the names of the files in the current folder.
 	/// </summary>
 	/// <remarks>This method performs a shallow query that returns only files in the current folder.</remarks>
-	/// <returns>When this method completes successfully, it returns a list of the names of the files in the current folder.</returns>
-	public Task<string[]> GetFileNamesAsync();
+	/// <returns>When this method completes, it returns a list of the names of the files in the current folder.</returns>
+	public Task<IFile[]> GetFilesAsync();
 
 	/// <summary>
-	/// Renames the file with the specified name in the current folder.
+	/// Creates a new subfolder with the specified name in the current folder.
 	/// </summary>
-	/// <param name="name">The name of the file to be renamed.</param>
-	/// <param name="newName">The desired, new name of the current item.</param>
-	/// <returns>If this method completes successfully, <see langword="true"/>. Otherwise <see langword="false"/>.</returns>
-	public Task<bool> RenameFileAsync(string name, string newName);
+	/// <param name="name">The name of the new subfolder to create in the current folder.</param>
+	/// <returns>When this method completes, it returns an <see cref="IFolder"/> that represents the new subfolder.</returns>
+	Task<IFolder> CreateFolderAsync(string name);
 }
