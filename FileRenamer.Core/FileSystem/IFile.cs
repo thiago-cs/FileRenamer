a4d4bf507833c75396b2ad7ed4537877a3ed9998
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-06-04 12:53:26 -0300

    Add IFile

---
+++ FileRenamer.Core/FileSystem/IFile.cs
@@ -0,0 +1,9 @@
+﻿namespace FileRenamer.Core.FileSystem;
+
+/// <summary>
+/// Represents a file in the target Operating System's file system.
+/// Provides information about the file, and ways to manipulate it.
+/// </summary>
+public interface IFile : IItem
+{
+}
