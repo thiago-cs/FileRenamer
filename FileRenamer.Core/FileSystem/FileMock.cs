88bbc8204abf4b0d89ccc770a216919b61142438
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-09-03 13:50:14 -0300

    Created file and folder mocks

---
+++ FileRenamer.Core/FileSystem/FileMock.cs
@@ -0,0 +1,48 @@
+﻿namespace FileRenamer.Core.FileSystem;
+
+/// <summary>
+/// Mocks the <see cref="IFile"/> from a file path as a string.
+/// </summary>
+public sealed class FileMock : IFile
+{
+	private string fileName;
+	private IFolder? folder;
+
+
+	public string Path => folder == null ? fileName : System.IO.Path.Combine(folder.Path, fileName);
+
+
+	public FileMock(string path)
+	{
+		fileName = System.IO.Path.GetFileName(path);
+
+		string? folderPath = System.IO.Path.GetDirectoryName(path);
+
+		folder = !string.IsNullOrEmpty(folderPath)
+			   ? new FolderMock(folderPath)
+			   : null;
+	}
+
+
+	public async Task<bool> RenameAsync(string newName)
+	{
+		await Task.CompletedTask;
+
+		if (newName == null)
+			return false;
+
+		fileName = newName;
+		return true;
+	}
+
+	public async Task<bool> MoveAsync(IFolder newFolder)
+	{
+		await Task.CompletedTask;
+
+		if (newFolder == null)
+			return false;
+
+		folder = newFolder;
+		return true;
+	}
+}
---
+++ FileRenamer.Core/FileSystem/FolderMock.cs
@@ -0,0 +1,78 @@
+﻿namespace FileRenamer.Core.FileSystem;
+
+/// <summary>
+/// Mocks the <see cref="IFolder"/> from a path as a string.
+/// </summary>
+public sealed class FolderMock : IFolder
+{
+	private readonly ICollection<IFolder> subFolders;
+	private readonly ICollection<IFile> subFiles;
+
+
+	public string Path { get; private set; }
+
+
+	public FolderMock(string path)
+	{
+		Path = path;
+		subFolders = new List<IFolder>();
+		subFiles = new List<IFile>();
+	}
+
+	public FolderMock(string path, ICollection<IFolder> subFolders, ICollection<IFile> subFiles)
+	{
+		Path = path;
+		this.subFolders = subFolders;
+		this.subFiles = subFiles;
+	}
+
+
+	public async Task<IFolder[]> GetSubfoldersAsync()
+	{
+		await Task.CompletedTask;
+
+		return subFolders.ToArray();
+	}
+
+	public async Task<IFile[]> GetFilesAsync()
+	{
+		await Task.CompletedTask;
+
+		return subFiles.ToArray();
+	}
+
+	public async Task<bool> RenameAsync(string newName)
+	{
+		await Task.CompletedTask;
+
+		Path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Path) ?? "", newName);
+		// todo: test whether this is allowed by a file table mock.
+		return true;
+	}
+
+	public async Task<bool> MoveAsync(IFolder newFolder)
+	{
+		await Task.CompletedTask;
+
+		string newPath = System.IO.Path.Combine(newFolder.Path, System.IO.Path.GetDirectoryName(Path)!);
+
+		// todo: test whether this is allowed by a file table mock.
+
+		IFolder[] subfolders = await newFolder.GetSubfoldersAsync();
+
+		IFolder subfolder = subfolders.FirstOrDefault()
+						 ?? await newFolder.CreateFolderAsync((this as IItem).Name);
+		return true;
+	}
+
+	public async Task<IFolder> CreateFolderAsync(string name)
+	{
+		await Task.CompletedTask;
+
+		string path = System.IO.Path.Combine(Path, name);
+		FolderMock newFolder = new(path);
+		subFolders.Add(newFolder);
+		// todo: add the relationship between folders to a file table mock.
+		return newFolder;
+	}
+}
