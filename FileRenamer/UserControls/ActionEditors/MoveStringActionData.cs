daecf2fe88195a7543f253e09e75bf25aa98a22c
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-09-04 14:09:12 -0300

    Added support for editing MoveStringAction's.

---
+++ FileRenamer/UserControls/ActionEditors/MoveStringActionData.cs
@@ -0,0 +1,101 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using CommunityToolkit.Mvvm.ComponentModel;
+using FileRenamer.Core.Indices;
+using FileRenamer.Core.Jobs.FileActions;
+using FileRenamer.UserControls.InputControls;
+
+
+namespace FileRenamer.UserControls.ActionEditors;
+
+public sealed partial class MoveStringActionData : ObservableValidator
+{
+	private const string ZeroCountErrorMessage = "Enter a value other than zero.";
+
+
+	#region Texts
+
+	[CustomValidation(typeof(MoveStringActionData), nameof(ValidateObservableValidator))]
+	public SearchTextData OldString { get; } = new();
+
+	private void OldString_PropertyChanged(object sender, PropertyChangedEventArgs e)
+	{
+		if (e.PropertyName == nameof(HasErrors))
+			ValidateProperty(OldString, nameof(OldString));
+	}
+
+	#endregion
+
+	#region Count
+
+	[ObservableProperty]
+	[NotifyDataErrorInfo]
+	[CustomValidation(typeof(MoveStringActionData), nameof(ValidateCount))]
+	private int _count;
+
+	[ObservableProperty]
+	private string _countErrorMessage;
+
+	#endregion
+
+
+	#region Constructors
+
+	public MoveStringActionData()
+	{
+		Initialize();
+	}
+
+	public MoveStringActionData(MoveStringAction action)
+	{
+		OldString.TextType = action.UseRegex ? TextType.Regex : TextType.Text;
+		OldString.IgnoreCase = action.IgnoreCase;
+		OldString.Text = action.Text;
+
+		Count = action.Count;
+
+		Initialize();
+	}
+
+	private void Initialize()
+	{
+		OldString.PropertyChanged += OldString_PropertyChanged;
+		ValidateAllProperties();
+	}
+
+	#endregion
+
+
+	#region Validation
+
+	public static ValidationResult ValidateObservableValidator(INotifyDataErrorInfo notifier, ValidationContext _)
+	{
+		return notifier.HasErrors
+			 ? new("This object has errors.")
+			 : ValidationResult.Success;
+	}
+
+	public static ValidationResult ValidateCount(int value, ValidationContext context)
+	{
+		MoveStringActionData data = context.ObjectInstance as MoveStringActionData;
+
+		if (value == 0)
+		{
+			data.CountErrorMessage = ZeroCountErrorMessage;
+			return new(ZeroCountErrorMessage);
+		}
+		else
+		{
+			data.CountErrorMessage = null;
+			return ValidationResult.Success;
+		}
+	}
+
+	#endregion
+
+
+	public MoveStringAction GetRenameAction()
+	{
+		return new(OldString.Text, OldString.IgnoreCase, OldString.TextType == TextType.Regex, Count);
+	}
+}
