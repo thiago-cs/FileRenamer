aaafcce967cd2bf05803f58166fe4bc042301859
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-09-04 19:31:50 -0300

    Added CreateKeyboardAccelerator function.
    ...as a helper function to derived classes.

--- FileRenamer/Models/UICommandBase.cs
+++ FileRenamer/Models/UICommandBase.cs
@@ -1,4 +1,8 @@
-﻿namespace FileRenamer.Models;
+﻿using Windows.System;
+using Microsoft.UI.Xaml.Input;
+
+
+namespace FileRenamer.Models;
 
 public abstract class UICommandBase
 {
@@ -10,31 +14,44 @@ public abstract class UICommandBase
 	/// <summary>
 	/// Gets or sets the label for this command.
 	/// </summary>
-	public string Label { get; set; }
+	public string Label { get; protected init; }
 
 	/// <summary>
 	/// Gets or sets a description for this command.
 	/// </summary>
-	public string Description { get; set; }
+	public string Description { get; protected init; }
 
 	/// <summary>
 	/// Gets or sets a glyph for this command.
 	/// </summary>
-	public Microsoft.UI.Xaml.Controls.IconSource IconSource { get; set; }
+	public Microsoft.UI.Xaml.Controls.IconSource IconSource { get; protected init; }
 
 	/// <summary>
 	/// Gets or sets the access key (mnemonic) for this command.
 	/// </summary>
-	public string AccessKey { get; set; }
+	public string AccessKey { get; protected init; }
 
 	/// <summary>
 	/// Gets the key combinations that invokes the action associated with this command.
 	/// </summary>
-	public Microsoft.UI.Xaml.Input.KeyboardAccelerator KeyboardAccelerator { get; internal init; }
+	public KeyboardAccelerator KeyboardAccelerator { get; protected init; }
 
 
 	/// <summary>
 	/// Notifies that the <see cref="ICommand.CanExecute"/> property has changed.
 	/// </summary>
 	public abstract void NotifyCanExecuteChanged();
+
+
+	// Helper function
+
+	protected static KeyboardAccelerator CreateKeyboardAccelerator(VirtualKeyModifiers? modifier, VirtualKey acceleratorKey)
+	{
+		KeyboardAccelerator keyboardAccelerator = new() { Key = acceleratorKey };
+
+		if (modifier != null)
+			keyboardAccelerator.Modifiers = modifier.Value;
+
+		return keyboardAccelerator;
+	}
 }
