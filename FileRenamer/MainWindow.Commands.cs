26395d7034d14ba7cfb2241cff6bf76390c9f7be
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-09-04 16:15:12 -0300

    Moved commands to a dedicated file.

---
+++ FileRenamer/MainWindow.Commands.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Controls;
+using FileRenamer.Core.Jobs;
+using FileRenamer.Core.Jobs.FileActions;
+using FileRenamer.Models;
+using FileRenamer.UserControls.ActionEditors;
+
+
+namespace FileRenamer;
+
+partial class MainWindow
+{
+	#region Project Commands
+
+	public readonly UICommand NewProjectCommand;
+	public readonly UICommand LoadProjectCommand;
+	public readonly UICommand SaveProjectCommand;
+
+
+	private void ExecuteNewProject()
+	{
+	}
+
+	private void ExecuteLoadProject()
+	{
+	}
+
+	private void ExecuteSaveProject()
+	{
+	}
+
+	#endregion
+
+	#region Manage existing actions commands
+
+	public readonly UICommand MoveUpActionCommand;
+	public readonly UICommand MoveDownActionCommand;
+	public readonly UICommand EditActionCommand;
+	public readonly UICommand DuplicateActionCommand;
+	public readonly UICommand RemoveActionCommand;
+	public readonly UICommand RemoveAllActionsCommand;
+
+
+	private async void EditSelectedAction()
+	{
+		//
+		if (ViewModel.SelectedAction == null)
+		{
+			// Oops!
+			return;
+		}
+
+		//
+		IActionEditor actionEditor = ViewModel.SelectedAction switch
+		{
+			InsertAction action => new InsertActionEditor(action),
+			RemoveAction action => new RemoveActionEditor(action),
+			ReplaceAction action => new ReplaceActionEditor(action),
+			ChangeRangeCaseAction action => new ChangeCaseActionEditor(action),
+			ChangeStringCaseAction action => new ChangeCaseActionEditor(action),
+			MoveStringAction action => new MoveStringActionEditor(action),
+			_ => null,
+		};
+
+		//
+		IJobItem item = await EditJobItemInDialog(actionEditor);
+
+		if (item == null)
+			return;
+
+		int index = ViewModel.Project.Jobs.IndexOf(ViewModel.SelectedAction);
+		ViewModel.Project.Jobs.RemoveAt(index);
+		ViewModel.Project.Jobs.Insert(index, item);
+	}
+
+	#endregion
+
+	#region Add new actions commands
+
+	public readonly UICommand AddInsertActionCommand;
+	public readonly UICommand AddInsertCounterActionCommand;
+	public readonly UICommand AddRemoveActionCommand;
+	public readonly UICommand AddReplaceActionCommand;
+	public readonly UICommand AddConvertCaseActionCommand;
+	public readonly UICommand AddMoveStringActionCommand;
+
+
+	private async void AddInsertAction()
+	{
+		await EditAndAddJobItem(new InsertActionEditor());
+	}
+
+	private async void AddInsertCounterAction()
+	{
+		InsertActionEditor actionEditor = new();
+		actionEditor.Data.ValueSourceType = UserControls.InputControls.ValueSourceType.Counter;
+
+		await EditAndAddJobItem(actionEditor);
+	}
+
+	private async void AddRemoveAction()
+	{
+		await EditAndAddJobItem(new RemoveActionEditor());
+	}
+
+	private async void AddReplaceAction()
+	{
+		await EditAndAddJobItem(new ReplaceActionEditor());
+	}
+
+	private async void AddConvertCaseAction()
+	{
+		await EditAndAddJobItem(new ChangeCaseActionEditor());
+	}
+
+	private async void AddMoveStringAction()
+	{
+		await EditAndAddJobItem(new MoveStringActionEditor());
+	}
+
+
+	private async Task<IJobItem> EditJobItemInDialog(IActionEditor actionEditor)
+	{
+		if (actionEditor == null)
+		{
+			// Oops!
+			return null;
+		}
+
+		//
+		dialog.DataContext = actionEditor;
+
+		ContentDialogResult result = await dialog.ShowAsync();
+
+		if (result != ContentDialogResult.Primary)
+			return null;
+
+		if (!actionEditor.IsValid)
+		{
+			// Oops!
+			return null;
+		}
+
+		return actionEditor.GetRenameAction();
+	}
+
+	private async Task EditAndAddJobItem(IActionEditor actionEditor)
+	{
+		IJobItem newAction = await EditJobItemInDialog(actionEditor);
+
+		if (newAction == null)
+			return;
+
+		ViewModel.Project.Jobs.Add(newAction);
+		ViewModel.SelectedAction = newAction;
+	}
+
+	#endregion
+
+	private void UpdateCommandStates()
+	{
+		MoveUpActionCommand.NotifyCanExecuteChanged();
+		MoveDownActionCommand.NotifyCanExecuteChanged();
+		EditActionCommand.NotifyCanExecuteChanged();
+		DuplicateActionCommand.NotifyCanExecuteChanged();
+		RemoveActionCommand.NotifyCanExecuteChanged();
+		RemoveAllActionsCommand.NotifyCanExecuteChanged();
+	}
+}
--- FileRenamer/MainWindow.xaml.cs
+++ FileRenamer/MainWindow.xaml.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Threading.Tasks;
 using Windows.System;
 using Windows.Storage.Pickers;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
-using FileRenamer.Core.Jobs;
-using FileRenamer.Core.Jobs.FileActions;
 using FileRenamer.Models;
 using FileRenamer.ViewModels;
-using FileRenamer.UserControls.ActionEditors;
+
 
 namespace FileRenamer;
 
@@ -53,7 +50,7 @@ public sealed partial class MainWindow
 		RemoveActionCommand = CreateCommand2("Remove", "Del", "Remove the selected action",
 											Symbol.Delete, null, VirtualKey.Delete, ViewModel.RemoveSelectedAction, ViewModel.CanExecuteWhenSelectedActionIsNotNull);
 
-		RemoveAllActionCommand = CreateCommand2("Clear", "", "Remove all actions",
+		RemoveAllActionsCommand = CreateCommand2("Clear", "", "Remove all actions",
 											Symbol.Clear, VirtualKeyModifiers.Control, VirtualKey.Delete, ViewModel.RemoveAllActions, ViewModel.CanExecuteWhenActionsIsNotEmpty);
 
 		// 2.3. Add new actions commands
@@ -73,7 +70,7 @@ public sealed partial class MainWindow
 												Symbol.Font, Control_Shift, VirtualKey.C, AddConvertCaseAction);
 
 		AddMoveStringActionCommand = CreateCommand3("Move Text", "M", "Move a text around",
-												(char)0xE8AB, Control_Shift, VirtualKey.M, AddConvertCaseAction);
+												(char)0xE8AB, Control_Shift, VirtualKey.M, AddMoveStringAction);
 
 		#endregion
 
@@ -138,162 +135,6 @@ public sealed partial class MainWindow
 	{
 		UpdateCommandStates();
 	}
-
-
-	#region Commands
-
-	#region Project Commands
-
-	public readonly UICommand NewProjectCommand;
-	public readonly UICommand LoadProjectCommand;
-	public readonly UICommand SaveProjectCommand;
-
-
-	private void ExecuteNewProject()
-	{
-	}
-
-	private void ExecuteLoadProject()
-	{
-	}
-
-	private void ExecuteSaveProject()
-	{
-	}
-
-	#endregion
-
-	#region Manage existing actions commands
-
-	public readonly UICommand MoveUpActionCommand;
-	public readonly UICommand MoveDownActionCommand;
-	public readonly UICommand EditActionCommand;
-	public readonly UICommand DuplicateActionCommand;
-	public readonly UICommand RemoveActionCommand;
-	public readonly UICommand RemoveAllActionCommand;
-
-
-	private async void EditSelectedAction()
-	{
-		//
-		if (ViewModel.SelectedAction == null)
-		{
-			// Oops!
-			return;
-		}
-
-		//
-		IActionEditor actionEditor = ViewModel.SelectedAction switch
-		{
-			InsertAction action => new InsertActionEditor(action),
-			RemoveAction action => new RemoveActionEditor(action),
-			ReplaceAction action => new ReplaceActionEditor(action),
-			ChangeRangeCaseAction action => new ChangeCaseActionEditor(action),
-			ChangeStringCaseAction action => new ChangeCaseActionEditor(action),
-			_ => null,
-		};
-
-		//
-		IJobItem item = await EditJobItemInDialog(actionEditor);
-
-		if (item == null)
-			return;
-
-		int index = ViewModel.Project.Jobs.IndexOf(ViewModel.SelectedAction);
-		ViewModel.Project.Jobs.RemoveAt(index);
-		ViewModel.Project.Jobs.Insert(index, item);
-	}
-
-	#endregion
-
-	#region Add new actions commands
-
-	public readonly UICommand AddInsertActionCommand;
-	public readonly UICommand AddInsertCounterActionCommand;
-	public readonly UICommand AddRemoveActionCommand;
-	public readonly UICommand AddReplaceActionCommand;
-	public readonly UICommand AddConvertCaseActionCommand;
-	public readonly UICommand AddMoveStringActionCommand;
-
-
-	private async void AddInsertAction()
-	{
-		await EditAndAddJobItem(new InsertActionEditor());
-	}
-
-	private async void AddInsertCounterAction()
-	{
-		InsertActionEditor actionEditor = new();
-		actionEditor.Data.ValueSourceType = UserControls.InputControls.ValueSourceType.Counter;
-
-		await EditAndAddJobItem(actionEditor);
-	}
-
-	private async void AddRemoveAction()
-	{
-		await EditAndAddJobItem(new RemoveActionEditor());
-	}
-
-	private async void AddReplaceAction()
-	{
-		await EditAndAddJobItem(new ReplaceActionEditor());
-	}
-
-	private async void AddConvertCaseAction()
-	{
-		await EditAndAddJobItem(new ChangeCaseActionEditor());
-	}
-
-
-	private async Task<IJobItem> EditJobItemInDialog(IActionEditor actionEditor)
-	{
-		if (actionEditor == null)
-		{
-			// Oops!
-			return null;
-		}
-
-		//
-		dialog.DataContext = actionEditor;
-
-		ContentDialogResult result = await dialog.ShowAsync();
-
-		if (result != ContentDialogResult.Primary)
-			return null;
-
-		if (!actionEditor.IsValid)
-		{
-			// Oops!
-			return null;
-		}
-
-		return actionEditor.GetRenameAction();
-	}
-
-	private async Task EditAndAddJobItem(IActionEditor actionEditor)
-	{
-		IJobItem newAction = await EditJobItemInDialog(actionEditor);
-
-		if (newAction == null)
-			return;
-
-		ViewModel.Project.Jobs.Add(newAction);
-		ViewModel.SelectedAction = newAction;
-	}
-
-	#endregion
-
-	private void UpdateCommandStates()
-	{
-		MoveUpActionCommand.NotifyCanExecuteChanged();
-		MoveDownActionCommand.NotifyCanExecuteChanged();
-		EditActionCommand.NotifyCanExecuteChanged();
-		DuplicateActionCommand.NotifyCanExecuteChanged();
-		RemoveActionCommand.NotifyCanExecuteChanged();
-		RemoveAllActionCommand.NotifyCanExecuteChanged();
-	}
-
-	#endregion
 
 
 	// Cannot be moved to a view-model because the dialog shown requires a window reference.
