ec97b9f2b1984eccff535cdf454c05f1a6ae744e
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-09-04 00:19:38 -0300

    Create RenameActionExensions.cs

---
+++ FileRenamer.Core Tester/RenameActionExensions.cs
@@ -0,0 +1,23 @@
+﻿using FileRenamer.Core.FileSystem;
+using FileRenamer.Core.Jobs;
+using FileRenamer.Core.Jobs.FileActions;
+using static FileRenamer.Core_Tester.Resources;
+
+
+namespace FileRenamer.Core_Tester;
+
+public static class RenameActionExensions
+{
+	public static string Run(this RenameActionBase action, string input)
+	{
+		return action.Run(input, NoContext);
+	}
+
+	public static string Run(this RenameActionBase action, string input, JobContext context)
+	{
+		JobTarget target = new(new FileMock(input), 0);
+		action.Run(target, context);
+
+		return target.NewFileName;
+	}
+}
