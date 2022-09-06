cb9fd32f4f7324a0b8c9b6bbd4de07a299390ab2
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-09-06 09:02:18 -0300

    Added parameter-full constructor.
    This allowed me to remove the init accessor from all but one property.
    Also made the method CreateKeyboardAccelerator a local function inside the constructor.

--- FileRenamer/Models/UICommandBase.cs
+++ FileRenamer/Models/UICommandBase.cs
@@ -1,5 +1,6 @@
 ﻿using Windows.System;
 using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Controls;
 
 
 namespace FileRenamer.Models;
@@ -14,12 +15,12 @@ public abstract class UICommandBase
 	/// <summary>
 	/// Gets or sets the label for this command.
 	/// </summary>
-	public string Label { get; protected init; }
+	public string Label { get; }
 
 	/// <summary>
 	/// Gets or sets a description for this command.
 	/// </summary>
-	public string Description { get; protected init; }
+	public string Description { get; }
 
 	/// <summary>
 	/// Gets the tooltip for this command.
@@ -56,34 +57,54 @@ public abstract class UICommandBase
 	/// <summary>
 	/// Gets or sets a glyph for this command.
 	/// </summary>
-	public Microsoft.UI.Xaml.Controls.IconSource IconSource { get; protected init; }
+	public IconSource IconSource { get; }
 
 	/// <summary>
 	/// Gets or sets the access key (mnemonic) for this command.
 	/// </summary>
-	public string AccessKey { get; protected init; }
+	public string AccessKey { get; }
 
 	/// <summary>
 	/// Gets the key combinations that invokes the action associated with this command.
 	/// </summary>
-	public KeyboardAccelerator KeyboardAccelerator { get; protected init; }
+	public KeyboardAccelerator KeyboardAccelerator { get; }
+
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="UICommandBase"/> class.
+	/// </summary>
+	/// <param name="description">The description for this command.</param>
+	/// <param name="label">The label for this command.</param>
+	/// <param name="accessKey">The access key (mnemonic) for this command.</param>
+	/// <param name="modifier">The virtual key that must be pressed along with the <paramref name="acceleratorKey"/> to activate this command.</param>
+	/// <param name="acceleratorKey">The key combinations that invokes the action associated with this command.</param>
+	/// <param name="icon">The icon for this command.</param>
+	public UICommandBase(string description, string label, string accessKey,
+						 VirtualKeyModifiers? modifier, VirtualKey acceleratorKey, IconSource icon)
+	{
+		Label = label;
+		Description = description;
+		IconSource = icon;
+		AccessKey = accessKey;
+		KeyboardAccelerator = CreateKeyboardAccelerator(modifier, acceleratorKey);
+
+
+		// Helper function
+
+		static KeyboardAccelerator CreateKeyboardAccelerator(VirtualKeyModifiers? modifier, VirtualKey acceleratorKey)
+		{
+			KeyboardAccelerator keyboardAccelerator = new() { Key = acceleratorKey };
+
+			if (modifier != null)
+				keyboardAccelerator.Modifiers = modifier.Value;
+
+			return keyboardAccelerator;
+		}
+	}
 
 
 	/// <summary>
 	/// Notifies that the <see cref="ICommand.CanExecute"/> property has changed.
 	/// </summary>
 	public abstract void NotifyCanExecuteChanged();
-
-
-	// Helper function
-
-	protected static KeyboardAccelerator CreateKeyboardAccelerator(VirtualKeyModifiers? modifier, VirtualKey acceleratorKey)
-	{
-		KeyboardAccelerator keyboardAccelerator = new() { Key = acceleratorKey };
-
-		if (modifier != null)
-			keyboardAccelerator.Modifiers = modifier.Value;
-
-		return keyboardAccelerator;
-	}
 }
