4f77cb6081f24abb0c16084dba3220391c6f3282
Thiago Siqueira <71464913+thiago-cs@users.noreply.github.com>
2022-09-13 23:36:48 -0300

    Exposed a reference to the main window.

--- FileRenamer/App.xaml.cs
+++ FileRenamer/App.xaml.cs
@@ -1,20 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Microsoft.UI.Xaml;
 
 
 namespace FileRenamer;
@@ -25,6 +9,7 @@ namespace FileRenamer;
 public sealed partial class App : Application
 {
 	private Window m_window;
+	public MainWindow Window => m_window as MainWindow;
 
 
 	/// <summary>
@@ -41,7 +26,7 @@ public sealed partial class App : Application
 	/// will be used such as when the application is launched to open a specific file.
 	/// </summary>
 	/// <param name="args">Details about the launch request and process.</param>
-	protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+	protected override void OnLaunched(LaunchActivatedEventArgs args)
 	{
 		m_window = new MainWindow();
 		m_window.Activate();
