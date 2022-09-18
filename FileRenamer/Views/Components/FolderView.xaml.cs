0c244f29f089752f4e90759ec503fe5d2f7c6699
Thiago Siqueira <thiagocs.cci@hotmail.com>
2022-09-18 17:07:53 -0300

    Created FolderView

---
+++ FileRenamer/Views/Components/FolderView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using FileRenamer.Core.Jobs;
+
+
+namespace FileRenamer.Views.Components;
+
+public sealed partial class FolderView : UserControl
+{
+	private IList<JobTarget> _items;
+	public IList<JobTarget> Items
+	{
+		get => _items;
+		set
+		{
+			_items = value;
+			ItemsControl.ItemsSource = _items;
+		}
+	}
+
+	public event EventHandler TestRequested;
+
+
+	public FolderView()
+	{
+		InitializeComponent();
+	}
+
+
+	private void TestNameMenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+	{
+		var menuItem = sender as MenuFlyoutItem;
+		TestRequested?.Invoke(menuItem.DataContext, EventArgs.Empty);
+	}
+}
