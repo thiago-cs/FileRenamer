0751110f18c761d58f52db4f156087a64a796ac9
Thiago Siqueira <thiagocs.cci@hotmail.com>
2022-09-19 15:27:26 -0300

    Create ConditionalJobItem.cs

---
+++ FileRenamer.Core/Jobs/Conditionals/ConditionalJobItem.cs
@@ -0,0 +1,12 @@
+﻿namespace FileRenamer.Core.Jobs.Conditionals;
+
+
+public abstract class ConditionalJobItem : ComplexJobItem
+{
+	protected ConditionalJobItem(JobCollection jobs)
+		: base(jobs)
+	{ }
+
+
+	public abstract bool TestTarget(JobTarget target);
+}
