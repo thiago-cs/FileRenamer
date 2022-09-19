beb2267a7df65d07730732d352f551c38c3541fb
Thiago Siqueira <thiagocs.cci@hotmail.com>
2022-09-19 14:26:17 -0300

    Removed unused usings.

--- FileRenamer/ViewModels/ValueSources/RandomStringValueSourceViewModel.cs
+++ FileRenamer/ViewModels/ValueSources/RandomStringValueSourceViewModel.cs
@@ -1,9 +1,4 @@
 ﻿using FileRenamer.Core.ValueSources;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace FileRenamer.ViewModels.ValueSources;
