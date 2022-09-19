fc2f9410b02ebb5197c05cf3e4e62ad9ce0dcd86
Thiago Siqueira <thiagocs.cci@hotmail.com>
2022-09-19 15:28:54 -0300

    Converted into class.

---
+++ FileRenamer.Core/Jobs/FileActions/FileActionJob.cs
@@ -0,0 +1,8 @@
+﻿namespace FileRenamer.Core.Jobs.FileActions;
+
+/// <summary>
+/// Represents an action performed over a file or a folder.
+/// </summary>
+public abstract class FileActionJob : JobItem
+{
+}
