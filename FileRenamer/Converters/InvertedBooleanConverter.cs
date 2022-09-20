a4a545f3d91fb2c33bdac8bbdb2ac24d733d3c3b
Thiago Siqueira <thiagocs.cci@hotmail.com>
2022-09-19 23:08:55 -0300

    Create InvertedBooleanConverter.cs

---
+++ FileRenamer/Converters/InvertedBooleanConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace FileRenamer.Converters;
+
+internal class InvertedBooleanConverter : Microsoft.UI.Xaml.Data.IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, string language)
+	{
+		return !(bool)value;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, string language)
+	{
+		return !(bool)value;
+	}
+}
